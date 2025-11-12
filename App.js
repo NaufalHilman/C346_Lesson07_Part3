28edbbeff96f0a5f465e66d55e2488aeca6d3f28
NaufalHilman <24043273@myrp.edu.sg>
2025-11-12 10:26:33 +0800

    Activity 4

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.parent}>
+            <View style={[styles.child, { backgroundColor: "skyblue" }]}>
+                <Text>Square 1</Text>
+            </View>
+            <View style={[styles.child, { backgroundColor: "mediumaquamarine" }]}>
+                <Text>Square 2</Text>
+            </View>
+            <View style={[styles.child, { backgroundColor: "crimson" }]}>
+                <Text>Square 3</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    parent: {
+        flex: 1,
+        backgroundColor: "whitesmoke",
+        marginTop: 30,
+
+        // centers all children horizontally & vertically
+        justifyContent: "center", // vertical center
+        alignItems: "center", // horizontal center
+
+        flexDirection: "row", // aligns squares in a row
+    },
+    child: {
+        width: 80,
+        height: 80,
+        marginHorizontal: 10, // adds space between squares
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
